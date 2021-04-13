b4cab3b8aaee580bf20213e92baf0f1f0dc111df
Sparky4825 <39426588+Sparky4825@users.noreply.github.com>
2021-04-12 21:54:00 -0400

    Initial Commit

---
+++ UltimateTicTacToe/UltimateTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateTicTacToeApp.swift
+//  UltimateTicTacToe
+//
+//  Created by Mason Raffo on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimateTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
